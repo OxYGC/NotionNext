0d95d1d7d1ae1ec101219473c7029df31d6df7bc
keygenee <keygenee@gmail.com>
2024-04-04 16:27:20 +0400

    heo big CATEGORY

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -25,9 +25,13 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: 'https://blog.yanggc.fun',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+//  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
+//  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
+//  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+
+  HEO_HERO_CATEGORY_1: { title: '技术福利', url: '/tag/技术福利' },
+  HEO_HERO_CATEGORY_2: { title: '优质羊毛', url: '/tag/优质羊毛' },
+  HEO_HERO_CATEGORY_3: { title: '精品资源', url: '/tag/精品资源' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
